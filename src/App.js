f746fb93babecc3ff931ca1087131b7e1892cb1f
Eliyahu Shvartov <sheveryosef@gmail.com>
2022-01-16 20:07:08 +0200

    add colorHelper file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Palatte from "./Palatte";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
+
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div>
       <Palatte {...seedColors[4]}/>
