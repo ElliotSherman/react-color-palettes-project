5f583743329500515d7d234d5bc6fde088ff490c
Eliyahu Shvartov <sheveryosef@gmail.com>
2022-01-16 21:14:04 +0200

    add rc-slider and styled the slider

--- src/Palatte.js
+++ src/Palatte.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css';
 import './Palette.css'
+import Slider, { Range } from 'rc-slider';
+
 
 class Palatte extends Component {
+    constructor(props){
+        super(props);
+        this.state = {level: 500}
+        this.updateLevel = this.updateLevel.bind(this);
+    }
+    updateLevel(level){
+        this.setState({level})
+    }
     render() {
-        const colorBoxes = this.props.colors.map(color => (
+        const {colors} = this.props.palette;
+        const {level} = this.state
+        const colorBoxes = colors[level].map(color => (
             <ColorBox 
-            background={color.color}
+            background={color.hex}
             name ={color.name}
             />
-        ))
+        ));
         return (
             <div className='Palette'>
+                <div className='slider'>
+                <Slider 
+                    defaultValue={this.state.level} 
+                    min={100} 
+                    max={900} 
+                    step={100} 
+                    onAfterChange={this.updateLevel}
+                />
+                </div>
+                
                 {/* nav here */}
                 <div className='Palette-colors'>
                     {colorBoxes}
