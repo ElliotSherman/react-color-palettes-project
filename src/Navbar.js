03c3e573184cdcb8def76e72d127290f4ceffbc1
Eliyahu Shvartov <sheveryosef@gmail.com>
2022-01-17 14:07:19 +0200

    add snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import 'rc-slider/assets/index.css';
 import Slider from 'rc-slider';
 import Select from '@mui/material/Select';
+
+import Snackbar from '@mui/material/Snackbar';
+import CloseIcon from '@mui/icons-material/Close';
+
 import './Navbar.css'
-import { MenuItem } from '@mui/material';
+import { IconButton, MenuItem } from '@mui/material';
 
 class Navbar extends Component {
     constructor(props){
         super(props);
-        this.state={format:'hex'}
+        this.state={format:'hex',open:false}
         this.handleFormatChange = this.handleFormatChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
     }
+
     handleFormatChange(e){
-        this.setState({format:e.target.value})
+        this.setState({format:e.target.value , open:true})
         this.props.handleFormatChange(e.target.value)
+    }
+    closeSnackbar(){
+        this.setState({open:false})
     }
     render() {
         const {level,updateLevel} = this.props;
@@ -44,6 +53,25 @@ class Navbar extends Component {
                             <MenuItem value='rgba'>RGBA - rgba(255,255,255,1.0) </MenuItem>
                         </Select>
                     </div>
+                    <Snackbar 
+                    anchorOrigin={{vertical:'bottom', horizontal: 'left'}}
+                    open={this.state.open}
+                    autoHideDuration={1500}
+                    message={<span>Format Changed Successfully!</span>}
+                    ContentProps={{
+                        "aria-describedby":"message-id"
+                    }}
+                    onClose={this.closeSnackbar}
+                    action={[
+                    <IconButton 
+                        onClick={this.closeSnackbar}
+                        color='inherit'
+                        key='close'
+                        aria-label='close'
+                        >
+                        <CloseIcon />
+                    </IconButton>]}
+                    />
             </header>
         );
     }
