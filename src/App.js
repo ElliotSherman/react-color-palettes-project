c515a686521aa1b497c3d0459aa52954e8d38603
Eliyahu Shvartov <sheveryosef@gmail.com>
2022-01-17 15:32:07 +0200

    add react-router-dom and create starting point routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Palatte from "./Palatte";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
+import {Routes ,Route, Outlet } from 'react-router-dom'
+import ColorBox from "./ColorBox";
 
 function App() {
   return (
     <div>
-      <Palatte palette={generatePalette(seedColors[4])}/>
+      {/* <Palatte palette={generatePalette(seedColors[4])}/> */}
+      <Routes>
+
+        <Route 
+          path='/' 
+          element={<Palatte palette={generatePalette(seedColors[4])}/>}
+          />
+
+          <Route path="palette/:id" element={<h1>Palette Color :id</h1>} />
+          
+      </Routes>
     </div>
   );
 }
