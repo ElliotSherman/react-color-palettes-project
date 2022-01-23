dd75807a1252ab8efcb6d2665e5652aa3fad9613
Eliyahu Shvartov <sheveryosef@gmail.com>
2022-01-23 16:44:20 +0200

    Add format selector

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import 'rc-slider/assets/index.css';
-import Slider, { Range } from 'rc-slider';
+import Slider from 'rc-slider';
 import './Navbar.css'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-function Navbar({level , setLevel}) {
+
+export default function Navbar({level , setLevel , format , changeFormat}) {
 return (
     <header className='Navbar'>
         <div className='logo'>
             <a href='#'>react-color-picker</a>
             <sub className='personal-logo'><span>by</span> Elliot Sherman</sub>
         </div>
-            <div className='slider-container'>
-                <span>Level :{level}</span>
-                <div className='slider'>
-                    <Slider 
-                    defaultValue={level} 
-                    min={100} 
-                    max={900} 
-                    step={100} 
-                    onAfterChange={e => setLevel(e)}
-                    />
-                </div>
+        <div className='slider-container'>
+            <span>Level :{level}</span>
+            <div className='slider'>
+                <Slider 
+                defaultValue={level} 
+                min={100} 
+                max={900} 
+                step={100} 
+                onAfterChange={e => setLevel(e)}
+                />
             </div>
+        </div>
+        <div className='select-container'>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">Format</InputLabel>
+        <Select
+          value={format}
+          label="Format"
+          onChange={changeFormat}
+        >
+          <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+          <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
+          <MenuItem value='rgba'>RGBA - rgba(255,255,255,1.0)</MenuItem>
+        </Select>
+      </FormControl>
+        </div>
     </header>
-);
+    );
 }
 
-export default Navbar;
--- src/Palatte.js
+++ src/Palatte.js
@@ -7,24 +7,31 @@ import './Palette.css'
 
 export default function Palatte (props) {
     const {colors} = props.palette;
+    const [level ,setLevel] = useState(500);
+    const [format, setFormat] = useState('hex')
 
-        const [level ,setLevel] = useState(500);
-        const colorBoxes = colors[level].map(color => (
-            <ColorBox 
-            background={color.hex}
-            name ={color.name}
-            />
-        ));
-        return (
-            <div className='Palette'>
-                <Navbar 
-                    level={level}
-                    setLevel={setLevel}
-                />
-                <div className='Palette-colors'>
-                    {colorBoxes}
-                </div>
-                {/* footer here */}
-            </div>
-        );
+    const changeFormat = (e) => {
+        setFormat(e.target.value)
     }
+
+    const colorBoxes = colors[level].map(color => (
+        <ColorBox 
+        background={color[format]}
+        name ={color.name}
+        />
+    ));
+    return (
+        <div className='Palette'>
+            <Navbar
+                format={format}
+                level={level}
+                setLevel={setLevel}
+                changeFormat={changeFormat}
+            />
+            <div className='Palette-colors'>
+                {colorBoxes}
+            </div>
+            {/* footer here */}
+        </div>
+    );
+}
