210fab5df882a03c9fba5f09d97aabc312f8bd22
Eliyahu Shvartov <sheveryosef@gmail.com>
2022-01-24 19:21:14 +0200

    Add useStyles to MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import seedColors from './seedColors';
 import {Link} from 'react-router-dom'
+import MiniPalette from './MiniPalette';
+
 function PaletteList(props) {
     const palettes = seedColors;
     return (
         <div>
+            <MiniPalette/>
             <h1>React color palette list</h1>
             {palettes.map(palette => (
                 <p>
