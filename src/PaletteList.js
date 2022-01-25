b291da2c1302c27bef91dcc96265932bf1cb90a1
Eliyahu Shvartov <sheveryosef@gmail.com>
2022-01-25 12:43:41 +0200

    Add styled() from MUI/system and style PaletteList.js

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import seedColors from './seedColors';
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 import MiniPalette from './MiniPalette';
+import {styled} from   "@mui/system"
+
+const MyStyles = {
+    Root:styled('div',{
+    name:'Root'
+    })({
+    backgroundColor:'blue',
+    height:'100%',
+    display:'flex',
+    alignItems:'flex-start',
+    justifyContent:'center',
+    }),
+    Container:styled('div',{
+        name:'Container'
+    })({
+        width:'50%',
+        display:'flex',
+        alignItems:'flex-start',
+        flexDirection:'column',
+        flexWrap:'wrap'
+    }),
+    Nav:styled('nav',{
+        name:'Nav'
+    })({
+        display:'flex',
+        width:'100%',
+        justifyContent:'space-between'
+    }),
+    Palettes:styled('div',{
+        name:'Palettes'
+    })({
+        boxSizing:'border-box',
+        width:'100%',
+        display:'grid',
+        gridTemplateColumns:'repeat(3,30%)',
+        gridGap:'5%'
+    })
+}
 
 function PaletteList(props) {
+    const {Root ,Container ,Nav, Palettes} = MyStyles
     const palettes = seedColors;
     return (
-        <div>
-            <h1>React color palette list</h1>
-            {palettes.map(palette => (
+        <Root>
+            <Container>
+                <Nav>
+                    <h1>React color palette list</h1>
+                </Nav>
+                <Palettes>
+                {palettes.map(palette => (
                 <MiniPalette {...palette} to={`/palette/${palette.id}`}>{palette.paletteName}</MiniPalette>
-            ))}
-        </div>
+                ))}
+                </Palettes>
+            </Container>
+        </Root>
     );
 }
 
